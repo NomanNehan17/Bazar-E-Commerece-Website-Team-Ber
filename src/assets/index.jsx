17de831cc72202277e8f40d92cd62f771ccd8377
Noman Nehan <c211017@ugrad.iiuc.ac.bd>
2023-12-10 01:24:38 +0600

    update 3rd part my noman

---
+++ src/assets/index.jsx
@@ -0,0 +1,9 @@
+import logoDark from "./logoDark.png";
+import logoLight from "./logoLight.png";
+import cart from "./cart.png";
+import paymentLogo from "./paymentLogo.png";
+import googleLogo from "./googleLogo.png";
+import githubLogo from "./githubLogo.png";
+import facebookLogo from './facebookLogo.png'; 
+
+export{logoDark, logoLight, cart, paymentLogo, googleLogo, githubLogo,facebookLogo };
